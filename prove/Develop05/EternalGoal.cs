cb3419d4b5bc13be64ffe609280b0d1effe6e9d1
wthiierry <waagner.thiierry@gmail.com>
2023-06-29 20:41:00 -0300

    One more file

--- prove/Develop05/EternalGoal.cs
+++ prove/Develop05/EternalGoal.cs
@@ -1,23 +1,26 @@
-using System;
+public class EternalGoal: Goal{
 
-class EternalGoal : Goals
-{
-    private int score;
-    private bool isComplete;
-    private string goalType;
-
-    public override bool IsItComplete()
-    {
-        return true;
+    public EternalGoal(bool isNew):base(isNew){
+        //using -1 to indicate it never ends.  Or max number of times to complete it is infinite
+        //if this was 1, it would compelte as soon as they completed 1 time (e.g. simple goal)
+        //Since it's eternal, I set to -1 so it's clear it never completes.  
+        base.SetNumMax(-1);
     }
 
-    public override int RecoderEvent()
-    {
-        return 1;
+    public override void RecordEvent(){
+        base.SetNumDone(1);
+        //capture the reward.  If a reward was earned it won't be an empty string.  else it'll be ""
     }
 
-    public override string ToCSVRecord()
+    public override string GetXofYSummary()
     {
-        return "trest";
+        //not used for eternal so setting to empty
+        return "";
+    }
+
+    //EXTRA:  Add infinity symbol for eternal goals
+    public override string GetCheckBox(){
+        //unique to eternal
+        return "\u221E";
     }
 }
